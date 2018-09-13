4256d922b271a4ab7a9cf379e070b7db88e90d69
vatunyou <42827005+vatunyou@users.noreply.github.com>
2018-09-13 15:04:34 +0700

    walkgame

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1536825730,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/พื้น-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
